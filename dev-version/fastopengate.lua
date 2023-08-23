f9136857803e9bfe59c8a74527b30032395a8bb5
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2023-08-23 15:39:06 +0500

    Update fastopengate.lua

--- dev-version/fastopengate.lua
+++ dev-version/fastopengate.lua
@@ -5,18 +5,18 @@ require 'lib.moonloader'
 local vkeys = require "vkeys"
 local ini = require "inicfg"
 local nCfg = "FastOpenGate.ini"
-local cfg = ini.load({opengate = { lock = false }}, nCfg)
+local cfg = ini.load({opengate = { lock = true }}, nCfg)
 local cmd_opengate = { [VK_H] = '/opengate' }
 
 function main()
     if not isSampfuncsLoaded() or not isSampLoaded() then return end
     while not isSampAvailable() do wait(100) end
         sampRegisterChatCommand("gate", function()
-		    cfg.opengate.lock = not cfg.opengate.lock
-	        if ini.save(cfg, nCfg) then
-		        sampAddChatMessage("{58c9b1}[FastOpenGate] " .. (cfg.opengate.lock and "{AAFFAA}ON" or "{FFAAAA}OFF"), 0xEEEEEE)
-		    end
-	    end)
+		cfg.opengate.lock = not cfg.opengate.lock
+		if ini.save(cfg, nCfg) then
+			sampAddChatMessage("{58c9b1}[FastOpenGate] " .. (cfg.opengate.lock and "{AAFFAA}ON" or "{FFAAAA}OFF"), 0xEEEEEE)
+		end
+	end)
         sampRegisterChatCommand("gateinfo", function ()
             sampShowDialog(6405, 'FastOpenGate by Ben Puls', 'FastOpenGate: ' .. (cfg.opengate.lock and "{AAFFAA}ON" or "{FFAAAA}OFF")..'\n\n\n{FFFFFF}������ ������ ��������� ����� �� "H".', 'Close', '', DIALOG_STYLE_MSGBOX)
         end)
@@ -24,7 +24,7 @@ function main()
         if cfg.opengate.lock then
             for k, cmd in pairs(cmd_opengate) do
                 if isKeyJustPressed(k) then
-                    if not sampIsChatInputActive() and not sampIsDialogActive() and not sampIsCursorActive() and not isSampfuncsConsoleActive() then
+                    if not sampIsCursorActive() and not isSampfuncsConsoleActive() then
                         sampSendChat(cmd)
                     end
                 end
